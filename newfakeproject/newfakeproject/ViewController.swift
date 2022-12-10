5301501946516472c711879e5f111b25be48c668
Kiny <89192734912nik@gmail.com>
2022-12-10 16:41:48 +0300

    newcode

---
+++ newfakeproject/newfakeproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newfakeproject
+//
+//  Created by nikita on 10.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
